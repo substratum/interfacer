eae8c9606df103b28d5f9da84372f873276da34f
George G <kreach3r@users.noreply.github.com>
2016-08-20 21:16:29 +0300

    Makefile cleanup
    There is no need for calling multiple files
    
    Change-Id: I46afc11d0f09ff78a979f9b04ed0a754f866d1a1

--- Android.mk
+++ Android.mk
@@ -1,5 +1,23 @@
-# Include the Android.mk in app/src/main
+#
+# Inline Aosp Makefile for Masquerade
+#
+LOCAL_PATH:= $(call my-dir)
 
-LOCAL_DIR := $(call my-dir)
+include $(CLEAR_VARS)
 
-include $(LOCAL_DIR)/app/src/main/Masquerade.mk
+LOCAL_PACKAGE_NAME := masquerade
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
