bf57c2432cd329cea27fa8e7fe6b8bb92a45cd22
Surge1223 <surge1223@gmail.com>
2016-12-10 15:10:16 -0600

    OMS7-N: Allow Play Store to filter out OMS-only themes
    This will be used in the separation of OMS and Legacy themes, and gets
    added inside /system/etc/permissions.
    
    Themers: To utilize this to create a theme only viewable on an OMS ROM,
    add:
    
        <uses-feature
            android:name="projekt.substratum.theme"
            android:required="true" />
    
    to the AndroidManifest.xml in the theme's APK. Play Store will limit the
    theme to be viewed on OMS-only devices.
    
    Change-Id: Ibcda2fb3ecd4004d2a3c12560b4e7bdc07505703

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
+LOCAL_REQUIRED_MODULES := projekt.substratum.theme.xml
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
@@ -21,3 +22,11 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := projekt.substratum.theme.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
