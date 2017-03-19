31b36ca6276f1e972dfa6dcfc34abc1c8d54d457
George G <kreach3r@users.noreply.github.com>
2017-03-19 08:57:30 +0200

    Polish copyright
    Change-Id: I8e07870626b73f6d3e36f301f79a7d4d5fd897d1

--- app/src/main/java/projekt/interfacer/utils/IOUtils.java
+++ app/src/main/java/projekt/interfacer/utils/IOUtils.java
@@ -1,21 +1,19 @@
 /*
- * Copyright (c) 2017 Project Substratum
+ * Copyright (c) 2016-2017 Projekt Substratum
+ * This file is part of Substratum.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Substratum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Substratum is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * Also add information on how to contact you by electronic and paper mail.
- *
+ * along with Substratum.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package projekt.interfacer.utils;
